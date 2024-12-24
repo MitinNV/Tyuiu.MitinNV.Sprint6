e4c8fdd150f8dcd7d4360d5c281647a415748e1c
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-24 21:10:53 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint6.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.MitinNV.Sprint6.Task2.V1.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
