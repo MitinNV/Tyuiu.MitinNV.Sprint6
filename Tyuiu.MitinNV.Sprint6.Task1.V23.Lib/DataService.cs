da77fa05c011f4f9fb579de88a17a4f06e626ba6
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-24 20:08:34 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint6.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MitinNV.Sprint6.Task1.V23.Lib
+{
+    public class DataService : ISprint6Task1V23
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Sin(x) + (2*x/3) - Math.Cos(x) * 4 * x;
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
