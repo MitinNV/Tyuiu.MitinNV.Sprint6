3763e27a8b16613e1e3cea6d6a80ba7b68b65dd7
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-26 22:27:43 +0500

    .

--- Tyuiu.MitinNV.Sprint6.Task4.V25.Test/DataServiceTest.cs
+++ Tyuiu.MitinNV.Sprint6.Task4.V25.Test/DataServiceTest.cs
@@ -1,10 +1,15 @@
 ﻿namespace Tyuiu.MitinNV.Sprint6.Task4.V25.Test
 {
+    [TestClass]
     public class DataServiceTest
     {
+        [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 1);
+            int x = 1;
+            double y = Math.Round(Math.Cos(x) + (4.0 * x / 2.0) - Math.Sin(x) * 3.0 * x, 2);
+            var wait = 0.02;
+            Assert.AreEqual(wait, y);
         }
     }
 }
--- Tyuiu.MitinNV.Sprint6.Task4.V25/FormMain.cs
+++ Tyuiu.MitinNV.Sprint6.Task4.V25/FormMain.cs
@@ -19,5 +19,98 @@ namespace Tyuiu.MitinNV.Sprint6.Task4.V25
         }
 
         DataService ds = new DataService();
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            try
+            {
+                dataGridViewRes_MNV.Columns[0].Width = 100;
+                dataGridViewRes_MNV.Columns[1].Width = 100;
+                dataGridViewRes_MNV.Rows.Clear();
+                var x = Convert.ToInt32(textBoxValueX_MNV.Text);
+                var res = ds.GetMassFunction(x, Convert.ToInt32(textBoxValueY_MNV.Text));
+                foreach (var item in res)
+                {
+                    dataGridViewRes_MNV.Rows.Add(x, item);
+                    x++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void buttonInfoDone_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Спринт #6 | Таск #4 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void groupBoxResult_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxValueX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBoxValueX_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBoxCondition_MNV_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCond_MNV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelResult_MNV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonSaveToFile_MNV_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            try
+            {
+                var arr = ds.GetMassFunction(Convert.ToInt32(textBoxValueX_MNV.Text), Convert.ToInt32(textBoxValueY_MNV.Text));
+                var s = "F(x)\n";
+                foreach (var item in arr)
+                {
+                    s += $"{item}\n";
+                }
+
+                var path = Path.Combine(Path.GetTempPath(), "dataSprint6Task4.txt");
+
+                File.WriteAllText(path, s);
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка при сохранении в файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
