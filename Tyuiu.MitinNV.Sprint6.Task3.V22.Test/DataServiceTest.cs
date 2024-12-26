835a84e5327fac0b0c6a50a92d9c7f59e3e438a8
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-26 21:52:30 +0500

    .

--- Tyuiu.MitinNV.Sprint6.Task3.V22.Test/DataServiceTest.cs
+++ Tyuiu.MitinNV.Sprint6.Task3.V22.Test/DataServiceTest.cs
@@ -1,10 +1,15 @@
 ﻿namespace Tyuiu.MitinNV.Sprint6.Task3.V22.Test
 {
+    [TestClass]
     public class DataServiceTest
     {
+        [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 1);
+            int x = 1;
+            double y = Math.Round((2 * Math.Cos(x) + 2) / (2 * x - 1) + Math.Cos(x) - 5 * x + 3, 2);
+            var wait = 1.62;
+            Assert.AreEqual(wait, y);
         }
     }
 }
