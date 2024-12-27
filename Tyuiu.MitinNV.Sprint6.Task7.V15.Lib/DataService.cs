3e7674eb876e6ca505c6f448276bc46c66afbf3f
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-28 01:48:47 +0500

    .

--- Tyuiu.MitinNV.Sprint6.Task7.V15.Lib/DataService.cs
+++ Tyuiu.MitinNV.Sprint6.Task7.V15.Lib/DataService.cs
@@ -13,27 +13,27 @@ namespace Tyuiu.MitinNV.Sprint6.Task7.V15.Lib
 
             int[,] matrix = new int[rows, columns];
 
-            for (int i = 0; i < rows; i++)
-            {
-                string[] values = lines[i].Split(',');
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    string[] values = lines[i].Split(',');
 
-                for (int j = 0; j < columns; j++)
-                {
-                    if (int.TryParse(values[j], out int value))
-                    {
-                        matrix[i, j] = value;
-                    }
-                }
-            }
+            //    for (int j = 0; j < columns; j++)
+            //    {
+            //        if (int.TryParse(values[j], out int value))
+            //        {
+            //            matrix[i, j] = value;
+            //        }
+            //    }
+            //}
 
-            for (int i = 0; i < rows; i++)
-            {
-                int value = matrix[i, 6];
-                if (value < 10 && value % 3 == 0)
-                {
-                    matrix[i, 6] = 3;
-                }
-            }
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    int value = matrix[i, 6];
+            //    if (value < 10 && value % 3 == 0)
+            //    {
+            //        matrix[i, 6] = 3;
+            //    }
+            //}
 
             return matrix;
         }
