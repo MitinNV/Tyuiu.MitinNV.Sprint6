e9bdb8fd442f60eae34a9cb42e0636cb60112e33
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-28 01:47:24 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint6.Task7.V15.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MitinNV.Sprint6.Task7.V15.Lib
+{
+    public class DataService : ISprint6Task7V15
+    {
+        public int[,] GetMatrix(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            int rows = lines.Length;
+            int columns = lines[0].Split(',').Length;
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] values = lines[i].Split(',');
+
+                for (int j = 0; j < columns; j++)
+                {
+                    if (int.TryParse(values[j], out int value))
+                    {
+                        matrix[i, j] = value;
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                int value = matrix[i, 6];
+                if (value < 10 && value % 3 == 0)
+                {
+                    matrix[i, 6] = 3;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint6.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MitinNV.Sprint6.Task7.V15.Lib;
+
+namespace Tyuiu.MitinNV.Sprint6.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int i = 1;
+            foreach (var item in ds.GetMatrix("C:\\DataSprint5\\testSprint6Task7.csv"))
+            {
+                Console.Write($"{item} ");
+                if (i%7 == 0)
+                {
+                    Console.Write("\n");
+                }
+                i++;
+            }
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
