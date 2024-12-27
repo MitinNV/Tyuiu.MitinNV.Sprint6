7799faacebbd5954fa018a3679e219341411dfef
MitinNV <tyuiu.mitinnv@outlook.com>
2024-12-27 18:28:20 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint6.Task6.V29.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MitinNV.Sprint6.Task6.V29.Lib
+{
+    public class DataService : ISprint6Task6V29
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            StringBuilder result = new StringBuilder();
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    if (word.Contains('i'))
+                    {
+                        result.Append(word + " ");
+                    }
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint6.Task6.V29/FormMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.MitinNV.Sprint6.Task6.V29.Lib;
+
+namespace Tyuiu.MitinNV.Sprint6.Task6.V29
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+    }
+}
